f817d2701052b3e602bcd97f304118eb021e5e72
Mary <mariannachudinova@yandex.ru>
2025-01-18 22:24:12 +0300

    error and movie output

---
+++ src/components/Box/Box.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+
+export function Box({ children }) {
+  const [isHidden, setIsHidden] = useState(false);
+
+  return (
+    <div className="box">
+      <button
+        onClick={() => setIsHidden((prevState) => !prevState)}
+        className="btn-toggle"
+      >
+        {!isHidden ? "-" : "+"}
+      </button>
+      {!isHidden && <div>{children}</div>}
+    </div>
+  );
+}
--- src/components/Movies/Movie.jsx
+++ src/components/Movies/Movie.jsx
@@ -1,15 +1,22 @@
 import React from "react";
-import { Box } from "../Box";
+import { Box } from "../Box/Box";
 import { Error } from "../Error";
-import { List } from "../List";
+import { List } from "../List/List";
 import { MovieItem } from "./MovieItem";
+import { Spinner } from "../Spinner";
 
-export function Movie() {
+export function Movie({ isLoading, isError, movies }) {
   return (
     <Box>
-      <Error />
+      {isError && <Error />}
+      {isLoading && (
+        <div className="spinner-wrapper">
+          <Spinner />
+        </div>
+      )}
       <List className="list-movies">
-        <MovieItem />
+        {movies &&
+          movies.map((item, ind) => <MovieItem key={ind} movie={item} />)}
       </List>
     </Box>
   );
