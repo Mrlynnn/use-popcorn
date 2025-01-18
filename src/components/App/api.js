feff1f3554754f7b7824da0622cc4f35041975cf
mrlynnn <mariannachudinova@yandex.ru>
2025-01-18 22:24:12 +0300

    error and movie output

---
+++ src/components/App/api.js
@@ -0,0 +1,43 @@
+const API_KEY = "6cf63bf3";
+
+// export async function getMovies(query) {
+//   try {
+//     const resp = await fetch(
+//       `https://www.omdbapi.com/?apikey=${API_KEY}&s=${query}`
+//     );
+
+//     if (!resp.ok) throw new Error("Request error");
+
+//     const data = await resp.json();
+//     if (data.Response === "False") throw new Error("Can't find some movies 🥹");
+//     return data;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+export async function getMovies(query, controller, setIsLoading, setIsError) {
+  try {
+    setIsLoading(true);
+    setIsError(false);
+    const resp = await fetch(
+      `https://www.omdbapi.com/?apikey=${API_KEY}&s=${query}`,
+      {
+        signal: controller.signal,
+      }
+    );
+
+    if (!resp.ok) throw new Error("Request error");
+
+    const data = await resp.json();
+    setIsLoading(false);
+    if (data.Response === "False") throw new Error("Can't find some movies");
+    return data;
+  } catch (error) {
+    if (error.name === "AbortError") {
+      console.log("предыдущий запрос отменен");
+    } else {
+      setIsError(true);
+    }
+  }
+}
